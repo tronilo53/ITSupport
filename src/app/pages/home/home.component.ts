c2c7bc4709a583cb05f18f0826bec1e1a50a0959
Tronilo <Luis.Tronilo@henryschein.es>
2023-03-04 18:35:04 +0100

    Pequeñas modificaciones para la seleccion de idioma

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -35,9 +35,13 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
   }
   ngAfterViewInit(): void {
+    //Ocultar la ventana de: instalacion de avaya fallida
     this.renderer.addClass( this.avaya__fail.nativeElement, 'none' );
+    //Ocultar la ventana de: instalacion de avaya ok
     this.renderer.addClass( this.avaya__ok.nativeElement, 'none' );
+    //Ocultar la ventana de: progress bar, descarga de actualizacion
     this.renderer.addClass( this.modal.nativeElement, 'none' );
+    //Ocultar la ventana de: seleccion de idioma
     this.renderer.addClass( this.appLanguage.nativeElement, 'none' );
 
     this.checkAvayaInstall();
@@ -59,38 +63,62 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.__ipcService.on( 'setLanguage', ( event, args ) => {
       if( args.data === 'norRead' || args.data === 'notJson' || args.data === 'notWrite' ) {
         console.log( 'Ha habido un error al procesar el idioma, se establecerá como predeterminado el idioma: Español' );
+        this.renderer.addClass( this.appLanguage.nativeElement, 'none' );
+        this.renderer.removeClass( this.avaya__ok.nativeElement, 'none' );
       }else {
-        if( args.data === 'change__sp' ) console.log( 'Se ha cambiado al idioma: Español' );
-        else console.log( 'Se ha cambiado al idioma: Inglés' );
+        if( args.data === 'change__sp' ) {
+          console.log( 'Se ha cambiado al idioma: Español' );
+          this.renderer.addClass( this.appLanguage.nativeElement, 'none' );
+          this.renderer.removeClass( this.avaya__ok.nativeElement, 'none' );
+        }else {
+          console.log( 'Se ha cambiado al idioma: Inglés' );
+          this.renderer.addClass( this.appLanguage.nativeElement, 'none' );
+          this.renderer.removeClass( this.avaya__ok.nativeElement, 'none' );
+        }
       }
     });
   }
   //COMPROBAR QUE AVAYA ESTÉ INSTALADO
   private checkAvayaInstall(): void {
+    //Comunicacion entre procesos: verificar si avaya está instalado.
     this.__ipcService.send( 'checkAvayaInstall' );
     this.__ipcService.on( 'checkAvayaInstall', ( event, args ) => {
+      //Si avaya no está instalado...
       if( args.data === 'fail' ) {
+        //muestra la ventana de: Avaya no está instalado
         this.renderer.removeClass( this.avaya__fail.nativeElement, 'none' );
+        //Muestra una alerta diciendo que: Avaya no está instalado
         this.__alertService.alertError( 'No se ha encontrado Avaya One X Agent' );
+        //Si avaya está instalado...
       }else {
+        //Comunicación entre procesos: Verificar si hay un idioma en el archivo: language.xml
         this.__ipcService.send( 'checkLanguage' );
         this.__ipcService.on( 'checkLanguage', ( event, args ) => {
+          //Si no lee el archivo language.xml...
           if( args.data === 'notRead' ) {
+            //TODO: Establecer idioma a español.
             console.log( 'No se ha podido leer el archivo' );
+            //Mostrar ventana principal de avaya;
             this.renderer.removeClass( this.avaya__ok.nativeElement, 'none' );
+            //Si lee el archivo language.xml pero no procesa el JSON...
           }else if( args.data === 'notJson' ) {
+            //TODO: Establecer idioma a español.
             console.log( 'No se ha podido pasar a json' );
+            //Si lee el archivo language.xml y se procesa el JSON...
           }else {
+            //Si el idioma del archivo language.xml está vacío...
             if( args.data === '' ) {
+              //Muestra la ventana de selección de idioma.
               this.renderer.removeClass( this.appLanguage.nativeElement, 'none' );
-              //this.renderer.removeClass( this.avaya__ok.nativeElement, 'none' ); //TODO: Mostrar app
             }else if( args.data === 'sp' ) {
               console.log( 'El idioma está en español' );
+              //TODO: PONER APP EN ESPAÑOL
               this.renderer.removeClass( this.avaya__ok.nativeElement, 'none' );
               this.setVersion();
               this.checkUpdates();
             }else {
               console.log( 'El idioma está en inglés' );
+              //TODO: PONER APP EN INGLÉS
               this.renderer.removeClass( this.avaya__ok.nativeElement, 'none' );
               this.setVersion();
               this.checkUpdates();
